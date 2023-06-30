78e83b4995b6de6375da0712afab20c515a13c67
shavidiniatsi <shavidiniatsi@gmail.com>
2023-06-30 19:10:27 +0530

    Completed 4th content

---
+++ foodlabs/src/Data/OffersData.jsx
@@ -0,0 +1,28 @@
+const OffersData = [
+  {
+    id: 1,
+    title: "$2.55 off for new users",
+    description: "Reprehenderit in voluptate velit esse cillum dolore",
+    discountCode: "HelloFoodLabs",
+  },
+  {
+    id: 1,
+    title: "$2.55 off for new users",
+    description: "Reprehenderit in voluptate velit esse cillum dolore",
+    discountCode: "HelloFoodLabs",
+  },
+  {
+    id: 1,
+    title: "$2.55 off for new users",
+    description: "Reprehenderit in voluptate velit esse cillum dolore",
+    discountCode: "HelloFoodLabs",
+  },
+  {
+    id: 1,
+    title: "$2.55 off for new users",
+    description: "Reprehenderit in voluptate velit esse cillum dolore",
+    discountCode: "HelloFoodLabs",
+  },
+];
+
+export default OffersData;
--- foodlabs/src/Pages/Landing.jsx
+++ foodlabs/src/Pages/Landing.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router";
 import BurgerImage from "../Assets/BurgerImage.svg";
 import EasyToOrder from "../Assets/EasyToOrder.png";
@@ -8,7 +8,10 @@ import FavouritIcon from "../Assets/FavouritIcon.svg";
 import FasterDelivery from "../Assets/FasterDelivery.png";
 import FoodData from "../Data/FoodData";
 import Header from "../Components/Header";
-import "../Assets/Styles/Landing.css"; // Import the CSS file for animations
+import OfferImage  from "../Assets/OfferImage.svg"
+import "../Assets/Styles/Landing.css";
+import { Swiper, SwiperSlide } from 'react-id-swiper';
+import 'swiper/swiper-bundle.css';
 
 const FirstElement = () => {
   const navigate = useNavigate();
@@ -37,7 +40,7 @@ const FirstElement = () => {
             ad minim veniam, quis nostrud{" "}
           </p>
           <button
-            onClick={() => navigate("/login")}
+            onClick={() => navigate("/auth")}
             className="transition hover:-translate-y hover:red duration-200 ease-in-out bg-[#C40000] text-white rounded-3xl h-11 w-fit px-8 py-1 font-inter font-semibold fade-in"
           >
             Order Now
@@ -87,48 +90,247 @@ const SecondElement = () => {
 };
 
 const ThirdElement = () => {
-    return (
-      <div>
-        <div className="flex flex-row items-center justify-center px-36 mt-20 mx-48">
-          <p className="text-black font-inter font-bold text-6xl mt-4">
-            Our Special Dishes
-          </p>
-        </div>
-        <div className="flex flex-row items-start justify-between px-36 my-4">
-          {FoodData.slice(0, 4).map((dish, index) => (
-            <div
-              className="bg-[#ECECEC] rounded-xl shadow-lg p-3 relative mb-5 w-1/4 mx-5"
-              key={index}
-            >
-              <img src={Food} alt="Special Dish" className="rounded-lg mb-4" />
-              <div className="absolute top-1 right-0">
-                <img src={FavouritIcon} alt="Favorite" className="w-12 h-12" />
-              </div>
-              <p className="text-black font-inter font-bold text-[1.5rem]">
-                {dish.name}
-              </p>
-              <div className="flex flex-row items-center">
-                <div className="flex flex-col items-center">
-                  <p className="text-black font-inter font-bold text-[1.5rem] mt-4">
-                    {dish.price}
-                  </p>
-                </div>
-              </div>
-              <button
-                className="absolute bottom-3 right-3 bg-[#C40000] font-inter font-semibold text-white rounded-full px-4 py-2 text-sm"
-                onClick={() => {
-                  // Add to cart logic here
-                }}
-              >
-                Add To Cart
-              </button>
+  return (
+    <div>
+      <div className="flex flex-row items-center justify-center px-36 mt-20 mx-48">
+        <p className="text-black font-inter font-bold text-6xl mt-4 mb-10">
+          Our Special Dishes
+        </p>
+      </div>
+      <div className="flex flex-row items-start justify-between px-36 my-4">
+        {FoodData.slice(0, 4).map((dish, index) => (
+          <div
+            className="bg-[#ECECEC] rounded-xl p-3 relative mb-5 w-1/4 mx-2"
+            key={index}
+          >
+            <img src={Food} alt="Special Dish" className="rounded-lg mb-4" />
+            <div className="absolute top-1 right-0">
+              <img src={FavouritIcon} alt="Favorite" className="w-12 h-12" />
             </div>
-          ))}
+            <p className="text-black font-inter font-bold text-[1.5rem]">
+              {dish.name}
+            </p>
+            <div className="flex flex-row items-center">
+              <div className="flex flex-col items-center">
+                <p className="text-black font-inter font-bold text-[1.5rem] mt-4">
+                  {dish.price}
+                </p>
+              </div>
+            </div>
+            <button
+              className="absolute bottom-3 right-3 bg-[#C40000] font-inter font-semibold text-white rounded-full px-4 py-2 text-sm"
+              onClick={() => {
+                // Add to cart logic here
+              }}
+            >
+              Add To Cart
+            </button>
+          </div>
+        ))}
+      </div>
+      <p className="flex flex-row items-center justify-center mt-10 font-inter font-bold text-lg">
+        Explore More -
+      </p>
+    </div>
+  );
+};
+
+const FourthElement = () => {
+  const [isAllClicked, setIsAllClicked] = useState(true);
+  const [isBreakfastClicked, setIsBreakfastClicked] = useState(false);
+  const [isLunchClicked, setIsLunchClicked] = useState(false);
+  const [isDinnerClicked, setIsDinnerClicked] = useState(false);
+  const [isDessertsClicked, setIsDessertsClicked] = useState(false);
+  const [isBeveragesClicked, setIsBeveragesClicked] = useState(false);
+
+  const handleAllClick = () => {
+    setIsAllClicked((prevState) => !prevState);
+    setIsBreakfastClicked(false);
+    setIsLunchClicked(false);
+    setIsDinnerClicked(false);
+    setIsDessertsClicked(false);
+    setIsBeveragesClicked(false);
+  };
+
+  const handleBreakfastClick = () => {
+    setIsBreakfastClicked((prevState) => !prevState);
+    setIsAllClicked(false);
+    setIsLunchClicked(false);
+    setIsDinnerClicked(false);
+    setIsDessertsClicked(false);
+    setIsBeveragesClicked(false);
+  };
+
+  const handleLunchClick = () => {
+    setIsLunchClicked((prevState) => !prevState);
+    setIsAllClicked(false);
+    setIsBreakfastClicked(false);
+    setIsDinnerClicked(false);
+    setIsDessertsClicked(false);
+    setIsBeveragesClicked(false);
+  };
+
+  const handleDinnerClick = () => {
+    setIsDinnerClicked((prevState) => !prevState);
+    setIsAllClicked(false);
+    setIsBreakfastClicked(false);
+    setIsLunchClicked(false);
+    setIsDessertsClicked(false);
+    setIsBeveragesClicked(false);
+  };
+
+  const handleDessertsClick = () => {
+    setIsDessertsClicked((prevState) => !prevState);
+    setIsAllClicked(false);
+    setIsBreakfastClicked(false);
+    setIsLunchClicked(false);
+    setIsDinnerClicked(false);
+    setIsBeveragesClicked(false);
+  };
+
+  const handleBeveragesClick = () => {
+    setIsBeveragesClicked((prevState) => !prevState);
+    setIsAllClicked(false);
+    setIsBreakfastClicked(false);
+    setIsLunchClicked(false);
+    setIsDinnerClicked(false);
+    setIsDessertsClicked(false);
+  };
+
+  return (
+    <div>
+      <div className="flex flex-row items-center justify-center px-36 mt-10 mx-48">
+        <p className="text-black font-inter font-bold text-6xl mt-4 mb-10">
+          Popular Menu
+        </p>
+      </div>
+      <div className="px-40">
+        <button
+          className={`font-inter font-semibold rounded-full px-4 py-2 text-sm ${
+            isAllClicked
+              ? "bg-[#C40000] text-white"
+              : "text-black border-[#C40000] border-2"
+          } mr-5`}
+          onClick={handleAllClick}
+        >
+          All
+        </button>
+        <button
+          className={`font-inter font-semibold rounded-full px-4 py-2 text-sm ${
+            isBreakfastClicked
+              ? "bg-[#C40000] text-white"
+              : "text-black border-[#C40000] border-2"
+          } mr-5`}
+          onClick={handleBreakfastClick}
+        >
+          Breakfast
+        </button>
+        <button
+          className={`font-inter font-semibold rounded-full px-4 py-2 text-sm ${
+            isLunchClicked
+              ? "bg-[#C40000] text-white"
+              : "text-black border-[#C40000] border-2"
+          } mr-5`}
+          onClick={handleLunchClick}
+        >
+          Lunch
+        </button>
+        <button
+          className={`font-inter font-semibold rounded-full px-4 py-2 text-sm ${
+            isDinnerClicked
+              ? "bg-[#C40000] text-white"
+              : "text-black border-[#C40000] border-2"
+          } mr-5`}
+          onClick={handleDinnerClick}
+        >
+          Dinner
+        </button>
+        <button
+          className={`font-inter font-semibold rounded-full px-4 py-2 text-sm ${
+            isDessertsClicked
+              ? "bg-[#C40000] text-white"
+              : "text-black border-[#C40000] border-2"
+          } mr-5`}
+          onClick={handleDessertsClick}
+        >
+          Desserts
+        </button>
+        <button
+          className={`font-inter font-semibold rounded-full px-4 py-2 text-sm ${
+            isBeveragesClicked
+              ? "bg-[#C40000] text-white"
+              : "text-black border-[#C40000] border-2"
+          } mr-5`}
+          onClick={handleBeveragesClick}
+        >
+          Beverages
+        </button>
+      </div>
+      <div className="flex flex-row items-start justify-between px-36 my-4">
+        {FoodData.slice(0, 4).map((dish, index) => (
+          <div
+            className="bg-[#ECECEC] rounded-xl p-3 relative mb-5 w-1/4 mx-2"
+            key={index}
+          >
+            <img src={Food} alt="Special Dish" className="rounded-lg mb-4" />
+            <div className="absolute top-1 right-0">
+              <img src={FavouritIcon} alt="Favorite" className="w-12 h-12" />
+            </div>
+            <p className="text-black font-inter font-bold text-[1.5rem]">
+              {dish.name}
+            </p>
+            <div className="flex flex-row items-center">
+              <div className="flex flex-col items-center">
+                <p className="text-black font-inter font-bold text-[1.5rem] mt-4">
+                  {dish.price}
+                </p>
+              </div>
+            </div>
+            <button
+              className="absolute bottom-3 right-3 bg-[#C40000] font-inter font-semibold text-white rounded-full px-4 py-2 text-sm"
+              onClick={() => {}}
+            >
+              Add To Cart
+            </button>
+          </div>
+        ))}
+      </div>
+      <p className="flex flex-row items-center justify-center mt-10 font-inter font-bold text-lg">
+        Explore More -
+      </p>
+    </div>
+  );
+};
+
+const FifthElement = () => {
+  return (
+    <div>
+      <div className="flex flex-row items-center justify-center px-36 mt-20 mx-48">
+        <p className="text-black font-inter font-bold text-6xl mt-4 mb-10">
+          Offers
+        </p>
+      </div>
+      <div className="flex flex-row items-start justify-between px-36 my-4">
+        <div className="bg-[#ECECEC] rounded-xl shadow-xl p-3 relative mb-5 w-1/3 mx-2">
+          <div className="flex flex-row">
+            <img src={OfferImage} alt="Special Dish" className="rounded-lg h-32" />
+            <div className="flex flex-col ml-4 justify-center">
+              <p className="text-black font-inter font-bold text-[1.5rem]">
+              $2.55 off for new users
+              </p>
+              <p>
+              reprehenderit in voluptate velit esse cillum dolore
+              </p>
+              <p className="text-[#C40000]">
+              Use Code : HelloFoodLabs
+              </p>
+            </div>
+          </div>
         </div>
       </div>
-    );
-  };
-  
+    </div>
+  );
+};
 
 function Landing() {
   return (
@@ -137,6 +339,8 @@ function Landing() {
       <FirstElement />
       <SecondElement />
       <ThirdElement />
+      <FourthElement />
+      <FifthElement />
     </div>
   );
 }
